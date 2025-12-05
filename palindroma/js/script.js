b30df14d2ac03f7c943355179783bf157ddc4dc8
marcotristani <marco.tristani@hotmail.it>
2025-12-05 14:40:01 +0100

    flat:scritta logica esercizio palindroma

---
+++ palindroma/js/script.js
@@ -0,0 +1,9 @@
+//Chiedere a utente parola di tipo stringa
+//Creare funzione per verificare se la parola che gli forniamo è palindroma
+   // Prendere la parola fornita 
+   // Salvare la parola dell'utente in una variabile
+   // Salvare la parola invertita tramite ciclo for in una seconda variabili
+   //Verificare se queste due variabili sono uguali
+   // Se sono uguali allora la parola è palindroma altrimenti non è palindroma
+   // Far ritornare fuori dala funzione il risultato( o direttamente con un output proprio alla funzione oppure con un return)
+// Se risultato funzione torna con un return salvare questo valore in una variabile e stamparlo
