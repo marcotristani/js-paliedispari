7bc1146f9d1afb2a2e15bb00b3afd7f3ce13bc28
marcotristani <marco.tristani@hotmail.it>
2025-12-07 08:32:43 +0100

    flat : definita somma numberUser e numberPc e definito con funzione se pari o dispari

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,6 +1,13 @@
 let choiseUser = prompt("Scegliere pari o dispari").toLowerCase(); //Faccio scegliere all'utente tra pari o dispari
 let numberUser = parseInt(prompt("Inserire un numero da 1 a 5"));  //Faccio inserire all'utente un numero tra 1 e 5
 
-let numberpc = numberRandom(1 , 5);
+let numberPc = numberRandom(1 , 5);
 
-console.log(choiseUser, numberUser , numberpc);
+const sumUserPc = numberUser + numberPc;
+let sumEvenShots = "dispari ";
+
+if ( isEven( sumUserPc)) {
+    sumEvenShots = "pari"
+}
+
+console.log(choiseUser, numberUser , numberPc, sumUserPc, sumEvenShots);
